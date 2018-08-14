da99375a6a87059c0a880348fd6067b7952db53f
amy-liang <amy.liaxx@gmail.com>
2018-08-14 17:06:16 -0400

    Made hover for project cards, add img prop

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -4,12 +4,14 @@ class ProjectCard extends Component {
 
   render() {
     return (
-      <div className="project-card">
-        <div className="project-card__content">
-            <p className="project-card__title">{ this.props.title }</p>
-            <p className="project-card__text">{ this.props.description + " | " + this.props.time }</p>
+      <div className="project-card" style={{ backgroundImage: "url(" + this.props.img +")" }}>
+            <div className="over">
+              <p className="project-card__title">{ this.props.title }</p>
+              <p className="project-card__text">{ this.props.description + " | " + this.props.time }</p>
+            </div>
+            <div className="under">
+            </div>
         </div>
-      </div>
     );
   }
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ProjectCard from '../components/projectcard';
+import Background1 from '../img/background1.png';
 
 class Projects extends Component {
   render() {
     return (
       <div className="section__content projects" id="projects">
         <h1 className="section__title">Projects</h1>
-        <ProjectCard title="Portfolio" description="Personal Website" time="Current"/>
+        <ProjectCard title="Portfolio" description="Personal Website" time="Current" img={Background1}/>
         <ProjectCard title="GradeBot" description="Grade Calculator" time="2017"/>
         <ProjectCard title="Anthem" description="Song Generator" time="2017"/>
       </div>
